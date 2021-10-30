0e17f739ad6ca185c5abc41ffa005fe4015169ff
finnaminope <91102755+finnaminope@users.noreply.github.com>
2021-10-30 15:48:06 +0000

    a lot of stuff

--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
-print("lua is cool")
+--[[print("lua is cool")
+local function doge()
+    print ("test")
+end
+doge()]]--
+--print("doge".. "poop
+print("booting up")
+function wait(sec)
+  local start = os.time()
+  repeat until os.time() > start + sec
+end
+wait(2)
+while true do
+print("ready")
+str = io.read("*line")
+if string.match(str,"pdonl") then
+print("waiting for input")
+var = io.read("*line")
+print(var)
+else
+print("invalid str please try again")
+end
+end
+
