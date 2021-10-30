4cbff179a91ab5310f7ec1cda6269e5c7332815f
finnaminope <91102755+finnaminope@users.noreply.github.com>
2021-10-30 20:33:35 +0000

    ADDED ALL THR GOOD STUFF

--- main.lua
+++ main.lua
@@ -1,24 +1,30 @@
---[[print("lua is cool")
-local function doge()
-    print ("test")
-end
-doge()]]--
---print("doge".. "poop
 print("booting up")
+print("getting functions")
 function wait(sec)
-  local start = os.time()
-  repeat until os.time() > start + sec
+	local start = os.time()
+	repeat until os.time() > start + sec
 end
-wait(2)
-while true do
-print("ready")
-str = io.read("*line")
-if string.match(str,"pdonl") then
-print("waiting for input")
-var = io.read("*line")
-print(var)
-else
-str = ("'".. str .. "'")
-print(str.. " is not a valid command")
+print("done")
+lr = true
+inval = 0
+print("welcome to lua cli!")
+
+while lr do
+	print("ready")
+	str = io.read("*line")
+	if string.match(str,"pdonl") then
+		print("waiting for input")
+		var = io.read("*line")
+		print(var)
+	elseif string.match(str,"clr") then
+		print("press the x to clear the console")
+	elseif string.match(str,"error") then
+		error(1)
+	else
+		str = ("'".. str .. "'")
+		print(str.. " is not a valid command")
+			inval = inval + 1
+	
 end
 end
+assert (lr ,"ERROR CODE: " .. 0)
